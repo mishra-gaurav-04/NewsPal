0cabf541743181095c79395da3a2802d49f993f9
Gaurav Mishra <gaurav.info217@gmail.com>
2023-07-16 12:11:23 +0530

    First commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav className='p-5 bg-gray-800'>
+        <h1 className='text-white font-bold text-2xl'>NewsPal</h1>
+    </nav>
+  )
+}
+
+export default NavBar
